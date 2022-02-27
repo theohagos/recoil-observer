507aaf823881a290fc04f7369e1b6b65818e235c
Theo Hagos <theo.hagos@gmail.com>
2022-02-27 00:50:05 +0000

    addrecoil observer

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  useTabs: false,
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  proseWrap: "always"
+}
---
+++ src/components/RecoilObserver/RecoilObserver.tsx
@@ -0,0 +1,113 @@
+import {useEffect, useState} from 'react';
+import {Snapshot, useGotoRecoilSnapshot, useRecoilSnapshot} from 'recoil';
+
+const withDevTool =
+  process.env.NODE_ENV === 'development' &&
+  typeof window !== 'undefined' &&
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__;
+let devTool: any = null;
+
+interface RetainedSnapshot {
+  release: Function;
+  snapshot: Snapshot;
+}
+
+function RecoilObserver({
+  exclude = [],
+  name = 'Recoil State Observer',
+  maxAge = 100,
+  trace = true
+}: {
+  exclude?: string[];
+  name: string;
+  maxAge: number;
+  trace: boolean;
+}) {
+  const snapshot: Snapshot = useRecoilSnapshot();
+  const [snapshots, setSnapshots] = useState<Array<RetainedSnapshot>>([]);
+  const devToolsExtensions = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
+  const gotoSnapshot = useGotoRecoilSnapshot();
+  let unsubscribe: any = null;
+
+  const connect = () => {
+    if (!devTool) {
+      setSnapshots([]);
+      devTool = devToolsExtensions.connect({
+        name,
+        maxAge,
+        trace
+      });
+    }
+  };
+
+  const dispatch = (isModified: boolean) => {
+    let save = false;
+
+    // @ts-ignore
+    for (const node of snapshot.getNodes_UNSTABLE({isModified})) {
+      const {contents, state} = snapshot.getLoadable(node);
+      const {key} = node;
+
+      if (state === 'hasValue' && !key.includes('__withCallback') && !exclude.includes(key)) {
+        const action = {
+          type: key,
+          payload: {node, value: contents, snapshotId: snapshot.getID()}
+        };
+
+        devTool.send(action.type, action);
+        save = true;
+      }
+    }
+
+    if (save && snapshots?.every(({snapshot: s}) => s.getID() !== snapshot.getID())) {
+      setSnapshots([...snapshots, {release: snapshot.retain(), snapshot}]);
+    }
+  };
+
+  useEffect(() => {
+    if (withDevTool) {
+      connect();
+      devTool.init('init', true);
+      dispatch(false);
+    }
+
+    return () => {
+      unsubscribe?.();
+      devToolsExtensions.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (withDevTool) {
+      connect();
+      devTool.init('init', true);
+
+      if (!unsubscribe) {
+        unsubscribe = devTool?.subscribe(({type, payload, state}: any) => {
+          if (type !== 'START' && payload.type === 'JUMP_TO_ACTION') {
+            const {
+              payload: {snapshotId}
+            } = JSON.parse(state);
+
+            snapshots.forEach(({snapshot: s}) => {
+              if (s.getID() === snapshotId) {
+                gotoSnapshot(snapshot);
+              }
+            });
+          }
+        });
+      }
+
+      dispatch(true);
+    }
+
+    return () => {
+      unsubscribe?.();
+      devToolsExtensions.disconnect();
+    };
+  }, [snapshot]);
+
+  return null;
+}
+
+export default RecoilObserver;
